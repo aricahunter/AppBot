0dabb99fdddb66f6f8a11ac846c824ebf8ce4ed0
Douglas Yan <douglas.yan@originate.com>
2016-12-08 13:50:10 -0800

    Integrated call to Watson

--- index.js
+++ index.js
@@ -4,13 +4,29 @@
 
 var RtmClient = require('@slack/client').RtmClient;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var WatsonWrapper = require('./xmsbot_watson_wrapper.js')
 
 var bot_token = process.env.SLACK_KEY;
+var google_token = process.env.GOOGLE_SEARCH_KEY;
 
 var rtm = new RtmClient(bot_token);
 rtm.start();
+var context;
+WatsonWrapper.initConversation( function(error, responseContext) {
+  context = responseContext;
+});
 
-rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+rtm.on(RTM_EVENTS.MESSAGE, function (message) {
+  WatsonWrapper.sendMessage(message.text, context, function(err, response) {
+    if (err) {
+      rtm.sendMessage("Error asking watson", message.channel);
+    }
+    else{
+    // console.log(response.response);
+    context = response.context;
+    rtm.sendMessage(response.response, message.channel);
+    }
+  });
   //message.text is what the message is and what we'll want to feed to watson to figure out response
-  rtm.sendMessage("Hello!", message.channel);
+  
 });
---
+++ xmsbot_watson_wrapper.js
@@ -0,0 +1,65 @@
+var watson = require('watson-developer-cloud');
+var username = process.env.WATSON_USERNAME_KEY,
+    password = process.env.WATSON_PASSWORD
+
+var conversation = watson.conversation({
+    username: username,
+    password: password,
+    version: 'v1',
+    version_date: '2016-09-20'
+});
+
+initConversation = function(done) {
+    conversation.message({
+        workspace_id: process.env.WATSON_WORKSPACE,
+        input: {'text': ''},
+        context: {}
+        },  function(err, response){
+            if (err){
+                console.log('error');
+                done();
+            }
+
+            else{
+                done(null, response["context"]);
+            }
+    });
+}
+
+sendMessage = function(userMessage, context, done) {
+    conversation.message({
+        workspace_id: process.env.WATSON_WORKSPACE,
+        input: {'text': userMessage},
+        context: context
+        },  function(err, response){
+            if (err) {
+                console.log(err)
+                done(err);
+            }
+            else{
+                // console.log(response["output"]["text"]);
+                var responseArray = response["output"]["text"];
+                done(null, {
+                    context: response["context"],
+                    response: response["output"]["text"][responseArray.length - 1]
+                });
+            }
+    });
+}
+
+module.exports = {
+    initConversation: initConversation,
+    sendMessage: sendMessage
+};
+
+// app.on('start', function())
+
+
+
+
+// when required
+
+// WatsonApi = require('./xms_bot_wrapper')
+
+// watsonApi = new WatsonApi({conversation: true});
+
